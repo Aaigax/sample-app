4f98fc64f0196063f1d655b27761f8b025930c51
Aaiga <ala11_aiga@hotmail.com>
2020-09-25 13:44:07 +1000

    variable port

--- config/connection.js
+++ config/connection.js
@@ -11,11 +11,11 @@ var mysql = require('mysql');
 // });
 
 var connection = mysql.createConnection({
-  host: process.env('aws-sample.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com'),
-  user: process.env('admin'),
-  password: process.env('AlatinaX!83'),
-  database: process.env('aws-sample'),
-  port: process.env(3304),
+  host: 'aws-sample.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
+  user:'admin',
+  password: 'AlatinaX!83',
+  database: 'db_httpsnodejs',
+  port: 3306,
   multipleStatements: true
 });
 
