df9fff563d3ce70f1e5c4877b98bf4fd8f0c18e4
Aaiga <ala11_aiga@hotmail.com>
2020-09-25 12:29:10 +1000

    variable port

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'AlatinaX!83',
+//   database: 'db_httpsnodejs',
+//   multipleStatements: true
+// });
+
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'aws-sample.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
+  user: 'admin',
   password: 'AlatinaX!83',
-  database: 'db_httpsnodejs',
+  database: 'aws-sample',
   multipleStatements: true
 });
 
