47ab923d7ac28781720280c132eca267a8c85666
Aaiga <ala11_aiga@hotmail.com>
2020-11-02 12:23:16 +1100

    update hostname

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 // });
 
 var connection = mysql.createConnection({
-  host: 'aws-sample.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
+  host: 'aws-test-db.ch17u3pxo3ux.ap-southeast-2.rds.amazonaws.com',
   user: 'admin',
   password: 'AlatinaX!83',
   database: 'db_httpsnodejs',
